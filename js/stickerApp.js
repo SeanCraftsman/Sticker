ca53e0e91689104f320185103c52d38aaddb168d
Sean <shironlai@sina.com>
2016-12-30 01:30:00 +0800

    finish basic function:

--- js/stickerApp.js
+++ js/stickerApp.js
@@ -1,27 +1,11 @@
-var bindEventDrag = function(s) {
-	bindEvent(s, 'mousedown', function(event){
-		s.dataset.drag = 'true'
-		s.dataset.diffX=event.clientX-s.offsetLeft;
-		s.dataset.diffY=event.clientY-s.offsetTop;
-		changeZIndex(s)
+var bindEventAddNew = function() {
+	var button = e('.add-sticker')
+	bindEvent(button, 'click', function(){
+		var c = e('#sticker-container')
+		var todo = todoNew()
+		saveTodo(todo)
+		showTodoList()
 	})
-
-	var body = e('body')
-	bindEvent(body, 'mousemove', function(event) {
-		let drag = s.dataset.drag
-		let diffX = s.dataset.diffX
-		let diffY = s.dataset.diffY
-		if (drag == 'true') {
-			var clientX = event.clientX
-			var clientY = event.clientY
-			s.style.left = clientX - diffX + 'px'
-			s.style.top = clientY - diffY + 'px'
-		}
-	})
-	bindEvent(body, 'mouseup', function() {
-		removeDragAll()
-	})
-
 }
 
 var changeZIndex = function(s) {
@@ -38,18 +22,78 @@ var changeZIndex = function(s) {
 	}
 }
 
-var bindEventDragAll = function() {
-	var s = es('.sticker')
-	for(let i = 0; i < s.length; i++) {
-		bindEventDrag(s[i])
-	}	
+var bindEventDrag = function() {
+	var list = e('#sticker-container')
+	// 事件委托
+	bindEvent(list, 'mousedown', function(event){
+		let target = event.target
+		let s = target.closest('.sticker')
+		if( s != null) {
+			s.dataset.drag = 'true'
+			s.dataset.diffX = event.clientX - s.offsetLeft;
+			s.dataset.diffY = event.clientY - s.offsetTop;
+			changeZIndex(s)
+		}
+	})
+
+	var body = e('body')
+	bindEvent(body, 'mousemove', function(event) {
+		var list = es('.sticker')
+		for(var i = 0; i < list.length; i++) {
+			let s = list[i]
+			let drag = s.dataset.drag
+			if (drag == 'true') {
+				let diffX = s.dataset.diffX
+				let diffY = s.dataset.diffY
+				let clientX = event.clientX
+				let clientY = event.clientY
+				s.style.left = clientX - diffX + 'px'
+				s.style.top = clientY - diffY + 'px'
+			}	
+		}
+	})
+	bindEvent(body, 'mouseup', function() {
+		removeDragAll()
+		refreshTodosAll()
+	})
 }
 
 var removeDragAll = function() {
-	var s = es('.sticker')
-	for(let i = 0; i < s.length; i++) {
-		s[i].dataset.drag = 'false'
+	var list = es('.sticker')
+	for(let i = 0; i < list.length; i++) {
+		let s = list[i]
+		s.dataset.drag = 'false'
 	}
+}
+
+var refreshTodo = function(s, todo) {
+	var message = s.querySelector('.sticker-message').innerHTML
+	var title = s.querySelector('.sticker-title').innerHTML
+	var done = s.classList.contains('done') ? true : false
+	var deadline = s.querySelector('.finish-time').innerHTML
+	var x = s.style.left
+	var y = s.style.top
+	var z = s.style.zIndex
+	todo.message = message
+	todo.title = title
+	todo.done = done
+	todo.deadline = deadline
+	todo.x = x
+	todo.y = y
+	todo.z = z
+}
+
+var refreshTodosAll = function() {
+	var list = es('.sticker')
+	var result = []
+	for(var i = 0; i < list.length; i++) {
+		var s = list[i]
+		var id = s.dataset.id
+		var todo = getTodo(id)
+		refreshTodo(s, todo)
+		result.push(todo)
+	}
+	saveTodos(result)
 }
 
 var removeEditableAll = function() {
@@ -63,46 +107,39 @@ var removeEditableAll = function() {
 var notEditable = function(target, selectorList) {
 	for(var i = 0; i < selectorList.length; i++) {
 		let s = selectorList[i]
-		if(target.closest(s) != null) {
+		let t = target.closest(s)
+		if(t != null) {
+			t.setAttribute('contenteditable', 'true')
 			return false
 		}
 	}
 	return true
 }
 
-var bindEventEditable = function(selector) {
-	bindAll(selector, 'mousedown', function(event){
-		removeEditableAll()
-		var target = event.target.closest(selector)
-		target.setAttribute('contenteditable', 'true')
-	})
-}
-
-var bindEventEditableAll = function() {
+var bindEventEditable = function() {
 	var selectorList = ['.sticker-message', '.sticker-title', '.finish-time']
-
-	for (var i = 0; i < selectorList.length; i++) {
-		let s = selectorList[i]
-		bindEventEditable(s)
-	}
-
 	var body = e('body')
 	bindEvent(body, 'mousedown', function(event){
 		var target = event.target
 		if (notEditable(target, selectorList)) {
-			log('null')
 			removeEditableAll()
 		}
 	})
 }
 
 
-var __main = function() {
-	bindEventDragAll()
-	bindEventEditableAll()
+var initApp = function() {
+	showTodoList()
+	var list = es('.sticker')
+	var container = e('#sticker-container')
+	container.dataset.num = list.length
 }
 
-var isDragging = false
-var diffX=0
-var diffY=0
+var __main = function() {
+	initApp()
+	bindEventAddNew()
+	bindEventDrag()
+	bindEventEditable()
+}
+
 __main()
---
+++ js/stickerPage.js
@@ -0,0 +1,50 @@
+var todoTemplate = function(todo) {
+	var done = todo.done? 'done':''
+	var title = todo.title
+	var time = todo.date
+	var message = todo.message
+	var deadline = todo.deadline
+	var id = todo.id
+	var x = todo.x
+	var y = todo.y
+	var z = todo.z
+	var t = `
+	    <div class="sticker ${done}" data-drag='false' 
+	    	 data-diff-x='0' data-diff-y='0'
+	         style="z-index: ${z}; left: ${x}; top: ${y};"
+	         data-id=${id}>
+			<div class="sticker-title" spellcheck="false">
+				${title}
+			</div>
+			<span class="sticker-shut">×</span>
+			<div class="sticker-time">
+				<div class="deadline-title">deadline:</div>
+				<div class="finish-time">${deadline}</div>
+				<div class="create-time">time: ${time} </div>
+			</div>	
+			<div class="sticker-message" spellcheck="false">
+				${message}
+			</div>
+		</div>
+	`
+	return t 
+}
+
+
+var insertTodoList = function(todoList) {
+    var todoListDiv = e('#sticker-container')
+    // 清空现有的所有 todo
+    todoListDiv.innerHTML = ''
+    for (var i = 0; i < todoList.length; i++) {
+        var todo = todoList[i]
+        var t = todoTemplate(todo)
+        appendHtml(todoListDiv, t)
+    }
+}
+
+// 加载所有 todo 并且显示在界面上
+var showTodoList = function() {
+    var todoList = loadTodos()
+    insertTodoList(todoList)
+}
+
