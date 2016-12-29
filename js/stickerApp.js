b3d7149b90da197659045d989eb302626a508e7d
Sean <shironlai@sina.com>
2016-12-29 14:48:41 +0800

    note can be moved now

---
+++ js/stickerApp.js
@@ -0,0 +1,74 @@
+var bindEventDrag = function(s) {
+	bindEvent(s, 'mousedown', function(event){
+		s.dataset.drag = 'true'
+		s.dataset.diffX=event.clientX-s.offsetLeft;
+		s.dataset.diffY=event.clientY-s.offsetTop;
+	})
+
+	var body = e('body')
+	bindEvent(body, 'mousemove', function(event) {
+		let drag = s.dataset.drag
+		let diffX = s.dataset.diffX
+		let diffY = s.dataset.diffY
+		if (drag == 'true') {
+			log('drag:', drag)
+			var clientX = event.clientX
+			var clientY = event.clientY
+			s.style.left = clientX - diffX + 'px'
+			s.style.top = clientY - diffY + 'px'
+		}
+	})
+	bindEvent(body, 'mouseup', function() {
+		removeDragAll()
+	})
+
+}
+
+var bindEventDragAll = function() {
+	var s = es('.sticker')
+	for(let i = 0; i < s.length; i++) {
+		bindEventDrag(s[i])
+	}	
+}
+
+var removeDragAll = function() {
+	var s = es('.sticker')
+	for(let i = 0; i < s.length; i++) {
+		s[i].dataset.drag = 'false'
+	}
+}
+
+var removeEditableAll = function(selector) {
+	var list = es(selector)
+	for(let i = 0; i < list.length; i++) {
+		let item = list[i]
+		item.removeAttribute('contenteditable')
+	}
+}
+
+var bindEventEditable = function() {
+	bindAll('.sticker-message', 'click', function(event){
+		removeEditableAll('.sticker-message')
+		var target = event.target
+		target.setAttribute('contenteditable', 'true')
+	})
+
+	var body = e('body')
+	bindEvent(body, 'mousedown', function(event){
+		var target = event.target
+		if (!target.classList.contains('sticker-message')) {
+			removeEditableAll('.sticker-message')
+		}
+	})
+}
+
+
+var __main = function() {
+	bindEventDragAll()
+	bindEventEditable()
+}
+
+var isDragging = false
+var diffX=0
+var diffY=0
+__main()
